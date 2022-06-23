5ba5f2b04d4ff45fc52154088f01ded0b5896378
Douglasjin <douglas.24tlc@hotmail.com>
2022-06-23 15:41:13 -0300

    feature/CONTELE-3321: Telinha nova de nova transferencia, Output, emit, mudancas no scss criando variavel para cor, ngSubmit

--- bytebank/src/app/TesteCoding/testeNovaTransferencia.component.ts
+++ bytebank/src/app/TesteCoding/testeNovaTransferencia.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+//cuidado ao importar o EventEmitter, as vezes ele pega do lugar errado
 
 @Component({
   selector: 'nova-transferencia',
   templateUrl: './testeNovaTransferencia.component.html',
-  styleUrls: ['./testeNovaTransferencia.component.component.scss']
+  styleUrls: ['./testeNovaTransferencia.component.scss']
 })
-export class AppComponent {
+export class testeNovaTransferencia {
   title = 'bytebank';
+
+  @Output() aoTransFerir = new EventEmitter<any>(); //mandar valor desse component, para outros components
+  //se for possivel nunca deixar any em nada, sempre colocar um tipo especifico
+
+  valor: number;
+  destino: number;
+
+  transferir(){
+    this.aoTransFerir.emit({valor: this.valor, destino: this.destino})
+    console.log("teste event")
+
+  }
+
 }
--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { testeNovaTransferencia } from './TesteCoding/testeNovaTransferencia.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    testeNovaTransferencia,//com isso outros components pode usar esse component novo, pra isso acontecer temos que importar no module dele
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
