edc2b3fb9e595a1927864ba274a73b6214e7c651
Douglasjin <douglas.24tlc@hotmail.com>
2022-06-28 17:19:21 -0300

    feature/CONTELE-3321: Validando o campo valor para quando o valor do campo é 0 null ou indefinido

--- bytebank/src/app/TesteCoding/testeNovaTransferencia.component.ts
+++ bytebank/src/app/TesteCoding/testeNovaTransferencia.component.ts
@@ -11,14 +11,32 @@ export class testeNovaTransferencia {
 
   @Output() aoTransFerir = new EventEmitter<any>(); //mandar valor desse component, para outros components
   //se for possivel nunca deixar any em nada, sempre colocar um tipo especifico
+  @Output() valoresComErro = new EventEmitter<string>();
 
   valor: number;
   destino: number;
 
   transferir(){
-    this.aoTransFerir.emit({valor: this.valor, destino: this.destino})
-    console.log("teste event")
+    
+    if(this.ehValido()){
+      console.log("EHVALIDO?", this.ehValido())
+      this.aoTransFerir.emit({valor: this.valor, destino: this.destino});
+      this.limparCampos();
+    }
 
+  }
+
+  private ehValido(){
+    const valido = this.valor === 0 ? false : this.valor === undefined ? false : this.valor === null ? false : 100;
+    if(!valido){
+      this.valoresComErro.emit('Informe um valor válido');
+    }
+    return valido
+  }
+
+  limparCampos(){
+    this.valor = null;
+    this.destino = null;
   }
 
 }
--- bytebank/src/app/app.component.ts
+++ bytebank/src/app/app.component.ts
@@ -9,8 +9,25 @@ import { testeNovaTransferencia } from './TesteCoding/testeNovaTransferencia.com
 export class AppComponent {
   title = 'bytebank';
 
+  valorTransferencia: any;
+  //valorTransferencia: any =  {} //podemos resolver o problema do erro que ocorre no console quando a gente tentar pegar os valores daqui de dentro pelo html iniciando ele como chaves
+  //ou podemos resolver esse problema la no html usando o ?
+
+  ixibirModal: boolean;
+  mensagemErro: string;
+
   transferir($event){
-    console.log("passou? ",$event)
+    this.valorTransferencia = $event;
+    console.log("UIA",$event)
+    this.modalErro($event)
+  }
+
+  modalErro($event){
+    this.ixibirModal = $event === "Informe um valor válido" ? true : false;
+    console.log("mensagem DERRRO" ,$event)
+    console.log("mensagem DERRRO123" ,this.ixibirModal)
+    //this.ixibirModal = false;
+    //Informe um valor válido
   }
 
 }
